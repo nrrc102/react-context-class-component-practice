11ba6fb7297bcba2d2a75d49be8ab685f8eb1942
nrrc102 <mumuree10@gmail.com>
2021-03-31 16:58:35 +0800

    creating multiple context

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Booklist from './components/Booklist';
 import Navbar from './components/Navbar';
 import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './context/AuthContext';
 import ThemeContextProvider from './context/ThemeContext';
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar/>
-        <Booklist/>
-        <ThemeToggle/>
+        <AuthContextProvider>
+          <Navbar/>
+          <Booklist/>
+          <ThemeToggle/>
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
