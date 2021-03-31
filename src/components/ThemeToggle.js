d0b2bdda87c75f861876b8a0791548de18681150
nrrc102 <mumuree10@gmail.com>
2021-04-01 05:31:54 +0800

    multiple context using hooks

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React, {useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 
-export default class ThemeToggle extends Component {
-    static contextType = ThemeContext;
-
-    render() {
-        const {toggleTheme} = this.context;
-        return (
-            <div>
-                <button onClick={toggleTheme}>Toggle the Theme</button>
-            </div>
-        )
-    }
+const ThemeToggle = () => {
+    const {toggleTheme} = useContext(ThemeContext);
+    return(
+        <div>
+            <button onClick={toggleTheme}>Toggle the Theme</button>
+        </div>
+    )
 }
+
+export default ThemeToggle;
